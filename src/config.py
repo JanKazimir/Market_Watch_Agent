8e6a387ed073ea1957e5ff8e92c5c666b0d4f367
Jkzmr <96251811+JanKazimir@users.noreply.github.com>
2026-05-04 16:14:54 +0200

    updated config

--- src/config.py
+++ src/config.py
@@ -4,12 +4,24 @@
 ## Scrapping data dictionnary:
 # the data dictionnary used when scraping
 
-test = { 
+scraped_data = { 
         "timestamp" : 0,
-        "source":str,
-        
-        
-        
+        "source_name": str,
+        "source_link":str,
+        "bank": str,
+        "product_name": str,
+        "product_type": str,
+        "base_rate": float,
+        "loyalty_bonus": float,
+        "total_rate"
+        "is_regulated": bool,
+        "categories": str,
+        "conditions": str,
+        "minimum_deposit": int,
+        "maximum_deposit":int,
+        "management_cost": int,
+        "link_to_pdf": str,
+        "interest_rate_guarantee": bool,
         
         }
 
