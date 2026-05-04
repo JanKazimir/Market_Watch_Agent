7a6ed41b80061496e2d6a52c115bade4eb0dfa23
Jkzmr <96251811+JanKazimir@users.noreply.github.com>
2026-05-04 15:44:22 +0200

    added more text

---
+++ src/config.py
@@ -0,0 +1,16 @@
+# This is where we have the config:
+
+
+## Scrapping data dictionnary:
+# the data dictionnary used when scraping
+
+test = { 
+        "timestamp" : 0,
+        "source":str,
+        
+        
+        
+        
+        }
+
+ 
