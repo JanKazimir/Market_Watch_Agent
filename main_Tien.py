81b3852c36d08babbe2b442f346912c18733b557
Tien Hoang <hoanghanhtien97@gmail.com>
2026-05-08 11:51:48 +0200

    Tien's update

---
+++ main_Tien.py
@@ -0,0 +1,13 @@
+def main():
+    print("Hello from market-watch-agent!")
+
+
+if __name__ == "__main__":
+    main()
+
+from summary_utils import process_and_send_summary
+
+if __name__ == "__main__":
+    # Just run the engine
+    result = process_and_send_summary()
+    print(result)
